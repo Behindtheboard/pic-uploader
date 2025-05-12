9c822faaaf99244cb1bcb10ee112a8113e587bf0
Behindtheboard <jk2624@gmail.com>
2025-05-12 01:42:51 -0700

    Center elements

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import FileUploader from "./components/FileUploader";
 export default function App() {
   return (
     <>
-      <div style={{ maxWidth: 400, margin: "2rem auto", textAlign: "center" }}>
-        <h2>📷 Upload to My Drive</h2>
+      <div className="section" style={{ maxWidth: 400, margin: "2rem auto", textAlign: "center" }}>
+        <h2>📷 Upload to our drive!</h2>
         <FileUploader />
       </div>
 
-      <div style={{ padding: "2rem" }}>
+      <div className="section" style={{ padding: "2rem" }}>
         <h1>💌 Guest Gallery</h1>
         <Gallery />
       </div>
