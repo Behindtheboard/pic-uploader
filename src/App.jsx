132dcd90d342ce8cd197aee246927cc553ad53ee
Behindtheboard <jk2624@gmail.com>
2025-05-26 10:51:27 -0700

    center

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Gallery from "./components/Gallery";
 import FileUploader from "./components/FileUploader";
 import { useState } from "react";
 import scavengerImg from "./png/Scavenger Hunt.png";
-import scKoreanImg  from "./png/SC_Korean.png";
+import scKoreanImg from "./png/SC_Korean.png";
 
 export default function App() {
   const [refreshKey, setRefreshKey] = useState(0);
@@ -17,12 +17,20 @@ export default function App() {
         style={{ maxWidth: 400, margin: "2rem auto", textAlign: "center" }}
       >
         <h2>📷 Upload to our drive!</h2>
-        <p>Please be careful to make sure you upload the correct photos or videos!</p>
-        <p>For video upload, wait a couple of seconds once you tap add. The videos are being copied onto the preview for you to upload.</p>
+        <p>
+          Please be careful to make sure you upload the correct photos or
+          videos!
+        </p>
+        <p>
+            For video upload, wait a couple of seconds once you tap add. The
+            videos are being copied onto the preview for you to upload.
+        </p>
         <FileUploader onAllComplete={handleUploadsComplete} />
       </div>
-      <img className="schunt" src={scavengerImg} />
-      <img className="schunt" src={scKoreanImg} />
+      <div id="hunt-ctn">
+        <img className="schunt" src={scavengerImg} />
+        <img className="schunt" src={scKoreanImg} />
+      </div>
       <div className="section" style={{ padding: "2rem" }}>
         <h1>💌 Guest Gallery</h1>
         <Gallery refreshKey={refreshKey} />
